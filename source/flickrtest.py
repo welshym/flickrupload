a0bc39b2579f1eafdcff24460e27f4bb432ff4e1
Mark Welsh <mark.c.welsh@gmail.com>
2012-10-22 13:47:10 +0100

    Updated soln to fix the test failures. Recursive search for photo objects now correctly handles default parameters.

--- source/flickrtest.py
+++ source/flickrtest.py
@@ -1,20 +1,19 @@
 import myflickrupload
 import unittest
 import sys
+import os
 
 class TestFlickrFunctions(unittest.TestCase):
 
     def setUp(self):
         self.seq = range(10)
 
-    #@unittest.expectedFailure
+    #@unittest.expectedFailure not anymore
     def test_LocalSetDiff(self):
         """Check that the local set diff is correct"""
         
-        myflickrupload.testexecution = True
-        
-        localset = myflickrupload.myGetLocalSets("..\\test\\testdata")
-        localflickrset = myflickrupload.myGetLocalSets("..\test\\testdataflickr")
+        localset = myflickrupload.myGetLocalSets(os.path.abspath(os.path.join(os.getcwd(), '..', 'test\\testdata')))
+        localflickrset = myflickrupload.myGetLocalSets(os.path.abspath(os.path.join(os.getcwd(), '..', 'test\\testdataflickr')))
         testsetdelta = myflickrupload.myFindMissingPhotoObjects(localset, localflickrset)
 
         testdelta = ["test1", "test2"]
@@ -25,7 +24,7 @@ class TestFlickrFunctions(unittest.TestCase):
     def test_LocalSetSearch(self):
         """Check that the local set search is correct"""
         testlist = ["test", "test1", "test2", "misc"]
-        localset = myflickrupload.myGetLocalSets("C:\\Users\\mark.welsh\\Personal\\Flickr\\TestWork\\testdata")
+        localset = myflickrupload.myGetLocalSets(os.path.abspath(os.path.join(os.getcwd(), '..', 'test\\testdata')))
         
         localsetnames = []
         for localsetitem in localset:
